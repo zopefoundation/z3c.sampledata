8126024938ed214c04b6912e04fe48593e7d4803
Stephan Richter <stephan.richter@gmail.com>
2008-02-14 07:26:59 +0000

    Version bump.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 setup (
     name='z3c.sampledata',
-    version='0.1.0',
+    version='0.2.0dev',
     author = "Stephan Richter and the Zope Community",
     author_email = "zope-dev@zope.org",
     description = "Sampledata Generator",
