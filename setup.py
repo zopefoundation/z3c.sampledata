1c0d0a4d0017f81f256c6d5929c202ecc76d9a01
Adam Groszer <agroszer@mail.com>
2010-08-30 12:09:20 +0000

    Preparing release 0.4.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 setup (
     name='z3c.sampledata',
-    version='0.3.2dev',
+    version='0.4.0',
     author = "Stephan Richter and the Zope Community",
     author_email = "zope-dev@zope.org",
     description = "Sampledata Generator",
