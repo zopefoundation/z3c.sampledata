f288833b366e56b4d6e9738864e2d8a8c01aa4d4
Stephan Richter <stephan.richter@gmail.com>
2013-02-26 20:23:28 -0500

    Back to development: 1.0.1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 setup (
     name='z3c.sampledata',
-    version='1.0.0',
+    version='1.0.1.dev0',
     author = "Stephan Richter and the Zope Community",
     author_email = "zope-dev@zope.org",
     description = "Sampledata Generator",
